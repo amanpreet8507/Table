956b9427e6cd69ccc8df7d00545102474b5e2c0d
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-09-19 19:06:01 -0700

    first commit

--- Table/main.cpp
+++ Table/main.cpp
@@ -7,8 +7,19 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
+using namespace std;
+int main() {
     // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    cout << "integer square cube\n";
+    cout << 0 << "\t\t" << 0*0 << "\t\t" << 0*0*0 << "\t\n" ;
+    cout << 1 << "\t\t" << 1*1 << "\t\t" << 1*1*1 << "\t\n" ;
+    cout << 2 << "\t\t" << 2*2 << "\t\t" << 2*2*2 << "\t\n" ;
+    cout << 3 << "\t\t" << 3*3 << "\t\t" << 3*3*3 << "\t\n" ;
+    cout << 4 << "\t\t" << 4*4 << "\t\t" << 4*4*4 << "\t\n" ;
+    cout << 5 << "\t\t" << 5*5 << "\t\t" << 5*5*5 << "\t\n" ;
+    cout << 6 << "\t\t" << 6*6 << "\t\t" << 6*6*6 << "\t\n" ;
+    cout << 7 << "\t\t" << 7*7 << "\t\t" << 7*7*7 << "\t\n" ;
+    cout << 8 << "\t\t" << 8*8 << "\t\t" << 8*8*8 << "\t\n" ;
+    cout << 9 << "\t\t" << 9*9 << "\t\t" << 9*9*9 << "\t\n" ;
+    cout << 10 << "\t\t" << 10*10 << "\t\t" << 10*10*10 << "\t\n" ;
 }
